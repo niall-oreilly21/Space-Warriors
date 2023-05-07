95b704d84d2f8ddae64321b4162994bb75e0b484
niall-oreilly21 <91060319+niall-oreilly21@users.noreply.github.com>
2023-05-07 02:54:12 +0100

    :sparkles: Add Scene class to store all Game Objects
    This class stores game objects in a big list with sub lists of the game objects type and their category. This makes lookup for gameObjects alot faster if there is a massive amount of them in a Scene.

---
+++ Scene.py
@@ -0,0 +1,49 @@
+from GameObject import GameObjectType
+
+class Scene:
+    def __init__(self, name):
+        self.name = name
+        self.game_object_list = {
+            GameObjectType.Static: {},
+            GameObjectType.Dynamic: {},
+            }
+
+    def add(self, game_object):
+        if game_object.category not in self.game_object_list[game_object.type]:
+            self.game_object_list[game_object.type][game_object.category] = []
+            self.game_object_list[game_object.type][game_object.category].append(game_object)
+
+    def remove(self, game_object):
+        if game_object.type in self.game_object_list[game_object.category]:
+            self.game_object_list[game_object.type][game_object.category].remove(game_object)
+
+    def find_all_by_type(self, type):
+        result = []
+        for category in self.game_object_list[type]:
+            result.extend(self.game_object_list[type][category])
+        return result
+
+    def find_all_by_category(self, type, category):
+        if category in self.game_object_list[type]:
+            return self.game_object_list[type][category]
+        else:
+            return []
+
+    def remove_all_by_category(self, type, category):
+        if category in self.game_object_list[type]:
+            return self.game_object_list[type].remove(category)
+        else:
+            return []
+
+    def update(self, game_time):
+        for type in self.game_object_list.values():
+            for category in type.values():
+                for game_object in category:
+                    game_object.update(game_time)
+
+    def render(self, screen, game_time):
+        for type in self.game_object_list.values():
+            for category in type.values():
+                for game_object in category:
+                    game_object.render(screen, game_time)
+
