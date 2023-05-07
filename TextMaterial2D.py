5df97e33e603b34982b77891dca0768e6e2a3350
niall-oreilly21 <91060319+niall-oreilly21@users.noreply.github.com>
2023-05-07 02:59:16 +0100

    :sparkles: Add TextMaterial2D class which extends from Material2D class
    The TextMaterial2D is used to draw text on the screen in a particular font and with an offset.

---
+++ TextMaterial2D.py
@@ -0,0 +1,15 @@
+import pygame
+from pygame.math import Vector2
+from Material2D import Material2D
+
+class TextMaterial2D(Material2D):
+    def __init__(self, sprite_font, text, text_offset, color, layer_depth=0, origin=(0, 0), sprite_effects=None):
+        super().__init__(color, layer_depth, origin, sprite_effects)
+        self.sprite_font = sprite_font
+        self.text = text
+        self.text_offset = text_offset
+
+    def draw(self, surface, transform):
+        text_position = tuple(transform.position + Vector2(*self.text_offset))
+        surface.blit(self.sprite_font.render(self.text, True, self.color), text_position)
+
