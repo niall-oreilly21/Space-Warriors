1a20990224278183fbb0668df3bdd2597d4d1ac1
niall-oreilly21 <91060319+niall-oreilly21@users.noreply.github.com>
2023-05-26 02:15:51 +0100

    :sparkles: Add CameraManager class to store the active camera
    The CameraManager updates the camera game object components.
    
    It has a set_active_camera method to set the camera for the Scene.

---
+++ Engine/Managers/CameraManager.py
@@ -0,0 +1,77 @@
+import os
+from typing import Dict
+
+import pygame
+from pygame import Vector2
+
+from Engine.GameObjects.Components.Camera import Camera
+from Engine.GameObjects.GameObject import GameObject
+from Engine.Managers.IUpdateable import IUpdateable
+from Engine.Other.Enums.GameObjectEnums import GameObjectType
+
+
+class CameraManager(IUpdateable):
+    def __init__(self, screen, sceneManager):
+        self.__screen = screen
+        self.__active_camera = None
+        self.__active_game_object = None
+        self.__cameras: Dict[str, GameObject] = {}
+        self.__needs_redraw = True
+        self.__sceneManager = sceneManager
+        self.player_is_moving = False
+
+    @property
+    def active_camera_transform(self):
+        if self.__active_game_object is None:
+            raise ValueError("ActiveCamera not set! Call SetActiveCamera()")
+        return self.__active_game_object.transform
+
+    @property
+    def active_camera_name(self):
+        if self.__active_game_object is None:
+            raise ValueError("ActiveCamera not set! Call SetActiveCamera()")
+        return self.__active_game_object.name
+
+    @property
+    def active_camera(self):
+        if self.__active_camera is None:
+            raise ValueError("ActiveCamera not set! Call SetActiveCamera()")
+        return self.__active_camera
+
+    def add(self, camera):
+        id = camera.name.strip().lower()
+
+        if id in self.__cameras:
+            return False
+
+        self.__cameras[id] = camera
+        return True
+
+    def set_active_camera(self, id):
+        camera_game_object = None
+        id = id.strip().lower()
+
+        if id in self.__cameras:
+            camera_game_object = self.__cameras[id]
+
+        if camera_game_object is not None:
+            self.__active_camera = camera_game_object.get_component(Camera)
+            self.__active_game_object = camera_game_object
+
+        if self.__active_camera:
+            self.__set_viewport()
+
+    def __set_viewport(self):
+        self.__screen = pygame.display.set_mode((self.__active_camera.viewport.x, self.__active_camera.viewport.y))
+        self.__needs_redraw = True
+
+
+    def update(self, game_time):
+        self.__active_game_object.update(game_time)
+
+        game_objects = self.__sceneManager.active_scene.find_all_by_type(GameObjectType.Dynamic)
+
+
+        # Apply the offset to the game objects' positions or rendering
+
+
