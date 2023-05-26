106fa5c34ced99780ae4b1f6752cd0b28985bf57
niall-oreilly21 <91060319+niall-oreilly21@users.noreply.github.com>
2023-05-26 02:12:23 +0100

    :sparkles: Add Rigidbody2D class which updates the movement of a GameObject
    The Rigidbody2D does not have any forces enabled as the game we plan to make is not a side scroller. However, it is still a useful class as it determines how fast an object is moving due to the rigidbody velocity.

---
+++ Engine/GameObjects/Components/Physics/Rigidbody2D.py
@@ -0,0 +1,61 @@
+from pygame import Vector2
+from Engine.GameObjects.Components.Component import Component
+
+class Rigidbody2D(Component):
+    def __init__(self, name, mass=1.0, drag=0.0):
+        super().__init__(name)
+        self.__mass = mass
+        self.__drag = drag
+        self.__velocity = Vector2(0,0)
+        self.__acceleration = Vector2(0,0)
+        self.__time_scale = 0.01  # Time scaling factor for velocity and acceleration
+
+    @property
+    def mass(self):
+        return self.__mass
+
+    @property
+    def velocity(self):
+        return self.__velocity
+
+    @velocity.setter
+    def velocity(self, velocity):
+        self.__velocity = velocity
+
+    @mass.setter
+    def mass(self, value):
+        self.__mass = value
+
+    @property
+    def drag(self):
+        return self.__drag
+
+    @drag.setter
+    def drag(self, value):
+        self.__drag = value
+
+    @property
+    def velocity(self):
+        return self.__velocity
+
+    @velocity.setter
+    def velocity(self, value):
+        self.__velocity = value
+
+    def apply_force(self, force):
+        self.__acceleration += force / self.__mass
+
+    def update(self, game_time):
+        delta_time = game_time.elapsed_time
+
+        # Update velocity using acceleration and drag
+        self.__velocity += self.__acceleration * delta_time
+        self.__velocity -= self.__velocity * self.__drag * delta_time
+
+        # Update position using velocity
+        self._transform.position += self.__velocity * delta_time
+
+
+
+        # Reset acceleration for the next frame
+        #self.__acceleration = Vector2(0, 0)
