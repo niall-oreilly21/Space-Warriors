66659de1f653f21c7206bc1e7c1f7d881801b636
niall-oreilly21 <91060319+niall-oreilly21@users.noreply.github.com>
2023-05-27 01:01:28 +0100

    :hammer: Add repeat_count and is_infinite parameter to the take class
    If the is_infinite is false the Animator2D class will only play the animation for a set amount of times.

--- Engine/Graphics/Sprites/SpriteAnimator2D.py
+++ Engine/Graphics/Sprites/SpriteAnimator2D.py
@@ -5,16 +5,18 @@ from Engine.Graphics.Sprites.Sprite import Sprite
 class SpriteAnimator2D(Component):
     def __init__(self, name, animator_info, material, active_take, fps):
         super().__init__(name)
+        self.__repeat_count = None
+        self.__is_animation_complete = None
+        self.__is_infinite = None
         self.__animator_info = animator_info
         self.__current_frames = None
         self.__active_take = None
-        self.set_active_take(active_take)
         self.__material = material
         self.__fps = fps
         self.__current_frame = 0
         self.__elapsed_time = 0
-        self.__is_repeated = False
-        self.__is_animation_complete = False
+        self.set_active_take(active_take)
+
 
     @property
     def active_take(self):
@@ -23,6 +25,22 @@ class SpriteAnimator2D(Component):
     @property
     def is_animation_complete(self):
         return self.__is_animation_complete
+
+    def get_current_sprite(self):
+        if self.__current_frame < len(self.__current_frames):
+            return Sprite(self.__material.texture, self.__current_frames[self.__current_frame], self.__material.color)
+        else:
+            return Sprite(self.__material.texture, self.__current_frames[0], self.__material.color)
+    def set_active_take(self, active_take):
+        for animator_info in self.__animator_info:
+            if animator_info.active_take == active_take:
+                self.__current_frames = animator_info.frame_rects
+                self.__active_take = active_take
+                self.__is_infinite = animator_info.is_infinite
+                self.__is_animation_complete = False
+                self.__repeat_count = animator_info.repeat_count
+
+    # ...
 
     def update(self, game_time):
         if self.__active_take is None:
@@ -33,31 +51,33 @@ class SpriteAnimator2D(Component):
         # Add the elapsed time to the animation timer
         self.__elapsed_time += game_time.elapsed_time
 
+        # Set the current sprite on the material
+        if self.__current_frame < len(self.__current_frames):
+            self.__material.source_rect = self.__current_frames[self.__current_frame]
+        else:
+            self.__material.source_rect = self.__current_frames[0]
+
         # If the animation timer exceeds the frame duration, advance to the next frame
         if self.__elapsed_time >= frame_duration:
             self.__current_frame = (self.__current_frame + 1) % len(self.__current_frames)
             self.__elapsed_time -= frame_duration
 
+
             # Check if the animation has reached the end
             if self.__current_frame == 0:
-                if self.__is_repeated:
-                    # Animation is repeated, reset the animation complete flag
-                    self.__is_animation_complete = False
+                if not self.__is_infinite:
+
+                    if self.__repeat_count <= 1:
+
+                        # Animation completed the desired repeat count, set the animation complete flag
+                        self.__is_animation_complete = True
+                        self.__active_take = None
+                    else:
+                        # Animation still needs to be repeated, decrement the repeat count
+                        self.__repeat_count -= 1
+                        self.__current_frame = 0
+                        self.__elapsed_time = 0
                 else:
                     # Animation completed, set the animation complete flag
                     self.__is_animation_complete = True
                     self.__active_take = None
-
-        # Set the current sprite on the material
-        self.__material.source_rect = self.__current_frames[self.__current_frame]
-
-    def set_active_take(self, active_take):
-        for animator_info in self.__animator_info:
-            if animator_info.active_take == active_take:
-                self.__current_frames = animator_info.frame_rects
-                self.__active_take = active_take
-                self.__is_repeated = animator_info.is_repeated
-                self.__is_animation_complete = False
-
-    def get_current_sprite(self):
-        return Sprite(self.__material.texture, self.__current_frames[self.__current_frame], self.__material.color)
--- Engine/Other/Enums/ActiveTake.py
+++ Engine/Other/Enums/ActiveTake.py
@@ -3,4 +3,5 @@ from enum import Enum
 
 class ActiveTake(Enum):
     PLAYER_RUNNING = 0,
-    PLAYER_WALKING = 1
+    PLAYER_WALKING = 1,
+    COOK = 2
