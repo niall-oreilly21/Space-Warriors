fe6d97c68717b74240eac912b2cbe1fb24c6e3bd
niall-oreilly21 <91060319+niall-oreilly21@users.noreply.github.com>
2023-05-29 02:34:52 +0100

    :sparkles: Add EventData class to store event information

---
+++ Engine/Other/Enums/EventEnums.py
@@ -0,0 +1,10 @@
+class EventCategoryType:
+    SceneManager = 0
+    CameraManager = 1
+
+
+class EventActionType:
+    MainMenuScene = 0
+    GameMenuScene = 1
+    MenuCamera = 2
+    GameCamera = 3
