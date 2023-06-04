480b0a37553940841c4bc421f15436aa15b0baa5
niall-oreilly21 <91060319+niall-oreilly21@users.noreply.github.com>
2023-06-05 00:46:56 +0100

    :hammer: Add animator info for the Teleporter game object

--- App/Constants/GameObjectConstants.py
+++ App/Constants/GameObjectConstants.py
@@ -3,17 +3,19 @@ from random import randint
 import pygame
 from pygame import Vector2
 
+from App.Components.Colliders.TeleporterCollider import TeleporterCollider
 from Engine.GameObjects.Components.Physics.BoxCollider2D import BoxCollider2D
 from Engine.GameObjects.GameObject import GameObject
 from Engine.Graphics.Materials.TextureMaterial2D import TextureMaterial2D
 from Engine.Graphics.Renderers.Renderer2D import Renderer2D
+from Engine.Graphics.Renderers.SpriteRenderer2D import SpriteRenderer2D
+from Engine.Graphics.Sprites.SpriteAnimator2D import SpriteAnimator2D
 from Engine.Graphics.Sprites.Take import Take
 from Engine.Other.Enums.ActiveTake import ActiveTake
 from Engine.Other.Enums.GameObjectEnums import GameObjectType, GameObjectCategory
 from Engine.Other.Transform2D import Transform2D
 from Engine.Other.Enums.RendererLayers import RendererLayers
 
-tp_image = pygame.image.load("Assets/SpriteSheets/Tilesets/Obelisk.png")
 image = pygame.image.load("Assets/SpriteSheets/Tilesets/plain_tileset.png")
 ruins_image = pygame.image.load("Assets/SpriteSheets/Tilesets/Assets_source.png")
 rocks_image = pygame.image.load("Assets/SpriteSheets/Tilesets/Rocks_source.png")
@@ -25,11 +27,7 @@ class GameObjectConstants:
     layer = RendererLayers.WorldObjects
     tree_layer = RendererLayers.Tree
 
-    TELEPORTER = GameObject("Teleporter", Transform2D(Vector2(0, 0), 0, Vector2(1, 1)), GameObjectType.Dynamic,
-                            GameObjectCategory.Player)
-    object_frame = tp_image.subsurface(pygame.Rect(52, 23, 79, 204))
-    texture_material = TextureMaterial2D(object_frame, None, Vector2(0, 0), 255)
-    TELEPORTER.add_component(Renderer2D("Renderer-2", texture_material, layer))
+
     # TELEPORTER.add_component(BoxCollider2D("Box-3"))
 
     TALL_TREE = GameObject("Tree", Transform2D(Vector2(0, 0), 0, Vector2(3, 3)), GameObjectType.Static,
@@ -148,3 +146,38 @@ class GameObjectConstants:
     object_frame = image2.subsurface(pygame.Rect(217, 292, 47, 31))
     texture_material = TextureMaterial2D(object_frame, None, Vector2(0, 0), 255)
     BRIDGE.add_component(Renderer2D("Renderer-2", texture_material, layer))
+
+    class Teleporter:
+        __TELEPORTER_HEIGHT = 368
+        __TELEPORTER_WIDTH = 117
+        __TELEPORTER_Y = 1
+
+        __TELEPORTER_FRAME_RECTS = [
+            pygame.Rect(52 - 19, __TELEPORTER_Y, __TELEPORTER_WIDTH, __TELEPORTER_HEIGHT), pygame.Rect(231 - 8, __TELEPORTER_Y, __TELEPORTER_WIDTH, __TELEPORTER_HEIGHT), pygame.Rect(421 - 8, __TELEPORTER_Y, __TELEPORTER_WIDTH, __TELEPORTER_HEIGHT),
+            pygame.Rect(611 - 8, __TELEPORTER_Y, __TELEPORTER_WIDTH, __TELEPORTER_HEIGHT),pygame.Rect(812 - 19, __TELEPORTER_Y, __TELEPORTER_WIDTH, __TELEPORTER_HEIGHT), pygame.Rect(1002 - 19, __TELEPORTER_Y, __TELEPORTER_WIDTH, __TELEPORTER_HEIGHT),
+            pygame.Rect(1192 - 19, __TELEPORTER_Y, __TELEPORTER_WIDTH, __TELEPORTER_HEIGHT), pygame.Rect(1380 - 17, __TELEPORTER_Y, __TELEPORTER_WIDTH, __TELEPORTER_HEIGHT), pygame.Rect(1553, __TELEPORTER_Y, __TELEPORTER_WIDTH, __TELEPORTER_HEIGHT),
+            pygame.Rect(1744, __TELEPORTER_Y, __TELEPORTER_WIDTH, __TELEPORTER_HEIGHT), pygame.Rect(1934, __TELEPORTER_Y, __TELEPORTER_WIDTH, __TELEPORTER_HEIGHT), pygame.Rect(2124 - 2, __TELEPORTER_Y, __TELEPORTER_WIDTH, __TELEPORTER_HEIGHT),
+            pygame.Rect(2314, __TELEPORTER_Y, __TELEPORTER_WIDTH, __TELEPORTER_HEIGHT), pygame.Rect(2522 - 19, __TELEPORTER_Y, __TELEPORTER_WIDTH, __TELEPORTER_HEIGHT)
+        ]
+        __TELEPORTER = Take(ActiveTake.TELEPORT, __TELEPORTER_FRAME_RECTS, False, 1)
+
+        __TELEPORTER_IDLE_FRAME_RECT = [pygame.Rect(52 - 19, __TELEPORTER_Y, __TELEPORTER_WIDTH, __TELEPORTER_HEIGHT)]
+
+        __TELEPORTER_IDLE = Take(ActiveTake.TELEPORT_IDLE, __TELEPORTER_IDLE_FRAME_RECT)
+
+        TELEPORTER_ANIMATION_INFO = [__TELEPORTER, __TELEPORTER_IDLE]
+
+        __TELEPORTER_SPRITE_SHEET = pygame.image.load("Assets/SpriteSheets/teleporter.png")
+
+        MATERIAL_TELEPORTER = TextureMaterial2D(__TELEPORTER_SPRITE_SHEET, None, Vector2(0, 0), None)
+
+        TELEPORTER = GameObject("Teleporter", Transform2D(Vector2(0, 0), 0, Vector2(1, 1)), GameObjectType.Static,
+                                GameObjectCategory.Teleporter)
+
+
+        TELEPORTER.add_component(SpriteAnimator2D("Teleporter Sprite Animator", TELEPORTER_ANIMATION_INFO, MATERIAL_TELEPORTER, ActiveTake.TELEPORT_IDLE, 4))
+        TELEPORTER.add_component(SpriteRenderer2D("Renderer-2", MATERIAL_TELEPORTER, RendererLayers.WorldObjects))
+        teleporter_box_collider = BoxCollider2D("Teleporter Box Collider")
+        teleporter_box_collider.scale = Vector2(4, 1)
+        TELEPORTER.add_component(teleporter_box_collider)
+        TELEPORTER.add_component(TeleporterCollider("Teleporter Collider"))
