5f91aad7089768e1bb7660c9b40c2bfda26627ab
niall-oreilly21 <91060319+niall-oreilly21@users.noreply.github.com>
2023-06-07 01:18:39 +0100

    :fire: Remove Point class

--- Engine/GameObjects/Components/Physics/Point.py
+++
@@ -1,9 +0,0 @@
-class Point:
-    def __init__(self, bounds, data):
-        self.bounds = bounds
-        self.data = data
-
-    def __eq__(self, other):
-        if isinstance(other, Point):
-            return self.data.parent.name == other.data.parent.name
-        return False
