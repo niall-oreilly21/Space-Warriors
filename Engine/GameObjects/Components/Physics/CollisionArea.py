aa262562ef13d2892fce2cb32f1273534655316f
niall-oreilly21 <91060319+niall-oreilly21@users.noreply.github.com>
2023-06-07 01:17:49 +0100

    :hammer: Remove methods and add getters to CollisionArea class

--- Engine/GameObjects/Components/Physics/CollisionArea.py
+++ Engine/GameObjects/Components/Physics/CollisionArea.py
@@ -9,30 +9,22 @@ class CollisionArea:
             self.__y = y
             self.__width = width
             self.__height = height
-            self.__left = None
-            self.__right = None
-            self.__top = None
-            self.__bottom = None
 
         @property
-        def left(self):
+        def x(self):
             return self.__x
 
         @property
-        def right(self):
-            return self.__x + self.__width
-
-        @property
-        def boundary(self):
-            return pygame.Rect(self.__x, self.__y, self.__width, self.__height)
-
-        @property
-        def top(self):
+        def y(self):
             return self.__y
 
-        @property
-        def bottom(self):
-            return self.__y + self.__height
+        @x.setter
+        def x(self, x):
+            self.__x = x
+
+        @y.setter
+        def y(self, y):
+            self.__y = y
 
         @property
         def width(self):
@@ -42,22 +34,12 @@ class CollisionArea:
         def height(self):
             return self.__height
 
-        def set_x(self, x):
-            self.__x = x
+        @property
+        def boundary(self):
+            return pygame.Rect(self.__x, self.__y, self.__width, self.__height)
 
-        def set_y(self, y):
-            self.__y = y
 
-        def intersects_screen(self, range_rect):
-            return (
-                    self.__x + self.__width > range_rect.x - range_rect.width
-                    and self.__x < range_rect.x + range_rect.width
-                    and self.__y + self.__height > range_rect.y - range_rect.height
-                    and self.__y < range_rect.y + range_rect.height
-            )
 
-        def is_in_range(self, rect_one, rect_two, range_distance):
-            center1 = rect_one.center
-            center2 = rect_two.center
-            distance = math.hypot(center2[0] - center1[0], center2[1] - center1[1])
-            return distance <= range_distance
+
+
+
