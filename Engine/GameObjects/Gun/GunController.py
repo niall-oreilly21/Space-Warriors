80268eb52738fb877086c54c4b0bb1cfe143bb56
niall-oreilly21 <91060319+niall-oreilly21@users.noreply.github.com>
2023-06-08 19:50:51 +0100

    :hammer: Add follow_target method to GunController

--- Engine/GameObjects/Gun/GunController.py
+++ Engine/GameObjects/Gun/GunController.py
@@ -2,6 +2,7 @@ from pygame import Vector2
 
 from Engine.GameObjects.Components.FollowController import FollowController
 from Engine.GameObjects.Components.Physics.BoxCollider2D import BoxCollider2D
+from Engine.Graphics.Renderers.Renderer2D import Renderer2D
 
 
 class GunController(FollowController):
@@ -11,12 +12,16 @@ class GunController(FollowController):
 
     def start(self):
         self.__target_box_collider = self._target.get_component(BoxCollider2D)
+        self.__rb = self._target.get_component(Renderer2D)
 
     def update(self, game_time):
+       self._follow_target()
+
+    def _follow_target(self):
         if self.__target_box_collider:
             target_position = self.__target_box_collider.bounds.center
-            #displacement = target_position - self._parent.transform.position
-            self._parent.transform.position = Vector2(target_position[0],target_position[1])
+            self._parent.transform.position = Vector2(target_position[0] - self.__rb.material.source_rect.width / 2,
+                                                      target_position[1] - self.__rb.material.source_rect.height / 2)
 
     def clone(self):
         return GunController(self.name, self.target)
