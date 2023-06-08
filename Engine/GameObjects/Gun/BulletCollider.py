4867420dc86073e5f0d34254b508acf14b84b008
niall-oreilly21 <91060319+niall-oreilly21@users.noreply.github.com>
2023-06-08 04:29:35 +0100

    :sparkles: Add Bullet Collider class
    This class removes the bullet from the Active Scene and QuadTree if it collides with any object.
    
    It also damages any objects that are instances of IDamageable.

---
+++ Engine/GameObjects/Gun/BulletCollider.py
@@ -0,0 +1,24 @@
+from App.Constants.Application import Application
+from App.Constants.Constants import Constants
+from Engine.GameObjects.Components.Physics.BoxCollider2D import BoxCollider2D
+from Engine.GameObjects.Components.Physics.Collider import Collider
+from Engine.Managers.EventSystem.EventData import EventData
+from Engine.Other.Enums.EventEnums import EventCategoryType, EventActionType
+from Engine.Other.Enums.GameObjectEnums import GameObjectCategory
+from Engine.Other.Interfaces.IDamageable import IDamageable
+
+
+class BulletCollider(Collider):
+    def __init__(self, name):
+        super().__init__(name)
+
+    def handle_response(self, colliding_game_object):
+        if colliding_game_object.game_object_category is not GameObjectCategory.Rat:
+            Constants.EVENT_DISPATCHER.dispatch_event(EventData(EventCategoryType.CollisionManager, EventActionType.RemoveCollliderFromQuadTree, [self._parent.get_component(BoxCollider2D)]))
+            Application.ActiveScene.remove(self._parent)
+            if isinstance(colliding_game_object, IDamageable):
+                colliding_game_object.damage(self._parent.bullet_damage)
+
+
+    def clone(self):
+        return BulletCollider(self.name)
